d749231f3ed7f2b2f7931debd93ee261f66ef878
Dominic Hopton <d.hopton@codevoid.net>
2025-04-19 16:34:21 -0700

    Basic text entry & output layout

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import './style.css'
+
+function sourceTextChanged(ev: Event): void {
+    const source = ev.target as HTMLTextAreaElement;
+    State.ouputArea.innerText = source.value;
+}
+
+let State: {
+    sourceText: HTMLTextAreaElement,
+    ouputArea: HTMLDivElement
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    State = {
+        sourceText: document.querySelector("textarea.source-text")!,
+        ouputArea: document.querySelector("div.output-text-container")!,
+    }
+
+    State.sourceText.addEventListener("input", sourceTextChanged);
+});
